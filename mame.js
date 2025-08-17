3b5689f4983882bb4169dc64cce824bf27d6037b
backstab1935 <fiat4sat@protonmail.com>
2025-08-17 18:34:00 +0330

    Update mame.js

--- mame.js
+++ mame.js
@@ -25,6 +25,23 @@ javascript:(function() {
         }
     };
 
+    // NEW: Remove the specific SVG icon
+    const removeSvgIcon = () => {
+        // Find all SVG elements on the page
+        const svgs = document.querySelectorAll('svg');
+        // A unique fill color from one of the paths in the target SVG
+        const uniqueFillColor = "#DE5833"; 
+        
+        svgs.forEach(svg => {
+            // Check if this SVG contains a path with the unique fill color
+            const path = svg.querySelector(`path[fill="${uniqueFillColor}"]`);
+            if (path) {
+                // If it's the target SVG, remove it
+                svg.remove();
+            }
+        });
+    };
+
     // Remove known elements
     const removeElements = () => {
         // 1. Side menu button
@@ -86,7 +103,7 @@ javascript:(function() {
             
             if (iconImages.length > 0) {
                 const targetBase64 = "PHN2ZyBmaWxsPSJub25lIiB2aWV3Qm94PSIwIDAgOTYgOTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CsKgIDxwYXRoIGZpbGw9IiMzOTlGMjkiIGQ9Ik03My4zMjcgNTFjMS41NiAwIDMuMDA3LS44OTcgMy41MDItMi4zNzZBMjIuNTY2IDIyLjU2NiAwIDAgMCA3OCA0MS40NDRDNzggMjcuMzkyIDY1LjAxNiAxNiA0OSAxNmgtNHYuMjRjLTE0LjEyMyAxLjcxLTI1IDEyLjM0Mi0yNSAyNS4yMDQgMCA4LjMzIDQuNTYzIDE1LjcyNiAxMS42MTcgMjAuMzY4Ljg0NS41NTYgMS4xNCAxLjY3Mi42MTUgMi41MzNMMjguMTY5IDcxSDI1LjV2M2g0Yy4xMzEtLjAwMi4yNjYtLjAxOC40MDQtLjA1IDQuNi0xLjA3IDEyLjM0Mi0yLjk1MyAxOS40MzItNS4wNTcgMS4xMjMtLjMzNCAxLjkyLTEuMzA2IDIuMTg1LTIuNDQ3QzUzLjU4MiA1Ny41OTQgNjEuNTIgNTEgNzEgNTFoMi4zMjdaIi8+CsKgIDxwYXRoIGZpbGw9IiM2M0M4NTMiIGQ9Ik03MS44ODUgNTFBMjIuNjg0IDIyLjY4NCAwIDAgMCA3NCA0MS40NDRDNzQgMjcuMzkyIDYxLjAxNiAxNiA0NSAxNlMxNiAyNy4zOTIgMTYgNDEuNDQ0YzAgOC4zMyA0LjU2MyAxNS43MjYgMTEuNjE3IDIwLjM2OC44NDUuNTU2IDEuMTQgMS42NzIuNjE1IDIuNTMzbC00LjI0OCA2Ljk1N2MtLjgyNCAxLjM1LjM3IDMuMDA5IDEuOTIgMi42NDggNS44NDMtMS4zNiAxNi43NTgtNC4wMyAyNC45NjQtNi44MDIuMzM1LS4xMTMuNTgxLS4zOTcuNjYyLS43NDJDNTMuNjA2IDU3LjU3NSA2MS41MzUgNTEgNzEgNTFoLjg4NVoiLz4KwqAgPHBhdGggZmlsbD0iI0NDQyIgZD0iTTkyLjUwMSA1OWMuMjk4IDAgLjU5NS4xMi44MjMuMzU0LjQ1NC40NjguNDU0IDEuMjMgMCAxLjY5OGwtMi4zMzMgMi40YTEuMTQ1IDEuMTQ1IDAgMCAxLTEuNjUgMCAxLjIyNyAxLjIyNyAwIDAgMSAwLTEuNjk4bDIuMzMzLTIuNGMuMjI3LS4yMzQuNTI0LS4zNTQuODIyLS4zNTRoLjAwNVptLTEuMTY2IDEwLjc5OGgzLjQ5OWMuNjQxIDAgMS4xNjYuNTQgMS4xNjYgMS4yIDAgLjY2LS41MjUgMS4yLTEuMTY2IDEuMmgtMy40OTljLS42NDEgMC0xLjE2Ni0uNTQtMS4xNjYtMS4yIDAtLjY2LjUyNS0xLjIgMS4xNjYtMS4yWm0tMS45ODIgOC43NTRjLjIyNy0uMjM0LjUyNS0uMzU0LjgyMi0uMzU0aC4wMDZjLjI5NyAwIC41OTUuMTIuODIyLjM1NGwyLjMzMiAyLjRjLjQ1NS40NjcuNDU1IDEuMjMgMCAxLjY5N2ExLjE0NSAxLjE0NSAwIDAgMS0xLjY1IDBsLTIuMzMyLTIuNGExLjIyNyAxLjIyNyAwIDAgMSAwLTEuNjk3WiIvPgrCoCA8cGF0aCBmaWxsPSIjZmZmIiBmaWxsLXJubGU9ImV2ZW5vZGQiIGQ9Ik0zMSA0NmE1IDUgMCAxIDAgMC0xMCA1IDUgMCAwIDAgMCAxMFptMTktNWE1IDUgMCAxIDEtMTAgMCA1IDUgMCAwIDEgMTAgMFptMTQgMGE1IDUgMCAxIDEtMTAgMCA1IDUgMCAwIDEgMTAgMFoiIGNsaXAtcnVsZT0iZXZlbm9kZCIvPgo8L3N2Zz4=";
-                const newSrc = `data:image/svg+xml;base64,${'$'}{targetBase64}`;
+                const newSrc = `data:image/svg+xml;base64,${targetBase64}`;
                 
                 for (const img of iconImages) {
                     if (img.src !== newSrc) {
@@ -104,6 +121,7 @@ javascript:(function() {
     replaceTitle();
     removeElements();
     replaceImages();
+    removeSvgIcon(); // <-- Run the new function
 
     // Observe DOM changes to catch elements that load later
     const observer = new MutationObserver(() => {
@@ -111,6 +129,7 @@ javascript:(function() {
         removeElements();
         replaceImages();
         replaceText(document.body);
+        removeSvgIcon(); // <-- Run the new function on changes
     });
 
     if (document.body) {
